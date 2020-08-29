9285007ce6f75f3cef83cfb65095628cf9d3f303
Martita Apel <mtapel@uc.cl>
2020-08-29 12:58:49 -0400

    table

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,62 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+/* import axios from "axios";
+ */
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     currentUser: null,
+    ordenes: [
+      {
+        num_orden: 123445,
+        cliente: "Multitiendas Sigma SA",
+        monto: 1540000,
+        cant_productos: 160,
+        fecha_entrega: "2020-07-20T00:00:00.000Z",
+        avance_preparacion: 0,
+        estado: "Ingresado",
+      },
+      {
+        num_orden: 123444,
+        cliente: "Jugueteria Asimov SpA",
+        monto: 980000,
+        cant_productos: 45,
+        fecha_entrega: "2020-07-20T00:00:00.000Z",
+        avance_preparacion: 0.4,
+        estado: "Preparacion",
+      },
+      {
+        num_orden: 123443,
+        cliente: "Bazar Don Lalo LDTA",
+        monto: 2400000,
+        cant_productos: 120,
+        fecha_entrega: "2020-07-18T00:00:00.000Z",
+        avance_preparacion: 0.6,
+        estado: "Preparacion",
+      },
+      {
+        num_orden: 123441,
+        cliente: "Claudia Ingrid Romero",
+        monto: 1740000,
+        cant_productos: 70,
+        fecha_entrega: "2020-07-14T00:00:00.000Z",
+        avance_preparacion: 1,
+        estado: "Entregado",
+      },
+    ],
+    pagedResult: {
+      page: 1,
+      size: 10,
+      total: 4,
+    },
   },
   mutations: {
     CURRENT_USER(state, user) {
       state.currentUser = user;
+    },
+    GET_ORDENES(state, ordenes) {
+      state.ordenes = ordenes;
     },
   },
   actions: {
@@ -23,6 +70,17 @@ export default new Vuex.Store({
         }
       });
     },
+    /* getOrdenes({ commit }) {
+      axios
+        .get("http://157.230.190.251/api/v1/cmodels/secure/ordenes/")
+        .then((response) => {
+          console.log(response.data);
+          commit("GET_ORDENES", response.data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }, */
   },
   modules: {},
 });
--- src/views/Ordenes.vue
+++ src/views/Ordenes.vue
@@ -1,5 +1,118 @@
 <template>
-  <div class="about">
-    <h1>This is an products page</h1>
+  <div class="ordenes">
+    <h1>Órdenes</h1>
+    <table class="tabla_ordenes">
+      <thead>
+        <tr>
+          <th>Nº Orden</th>
+          <th>Cliente</th>
+          <th>Monto Orden</th>
+          <th>Cantidad Productos</th>
+          <th>Fecha entrega</th>
+          <th>Avance Preparación</th>
+          <th>Estado</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="orden in ordenes" :key="orden.id">
+          <td>{{ orden.num_orden }}</td>
+          <td>{{ orden.cliente }}</td>
+          <td>{{ orden.monto }}</td>
+          <td>{{ orden.cant_productos }}</td>
+          <td>{{ orden.fecha_entrega }}</td>
+          <td>{{ orden.avance_preparacion }}</td>
+          <td>
+            {{ orden.estado }}
+            <b-icon
+              v-show="orden.estado == 'Entregado'"
+              icon="exclamation-circle-fill"
+              variant="success"
+            ></b-icon>
+            <b-icon
+              v-show="orden.estado == 'Preparacion'"
+              icon="exclamation-circle-fill"
+              variant="warning"
+            ></b-icon>
+            <b-icon
+              v-show="orden.estado == 'Ingresado'"
+              icon="exclamation-circle-fill"
+              variant="info"
+            ></b-icon>
+          </td>
+
+          <td>
+            <b-button variant="success" class="text-right"
+              >Ver Detalle</b-button
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Ordenes",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["ordenes"]),
+  },
+  methods: {
+    /*     ...mapActions(["getOrdenes"]),
+     */
+    /* eliminarPatient(id) {
+      const confirmar = confirm("¿Seguro que quieres eliminar este paciente?");
+      if (confirmar) {
+        this.deletePatient(id);
+      }
+    },
+    editarPatient(id) {
+      this.updateEdit();
+      this.findPatient(id);
+    }, */
+  },
+  /* created() {
+    this.getOrdenes();
+  }, */
+};
+</script>
+
+<style>
+.ordenes {
+  background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
+  background-size: cover;
+  min-width: 100%;
+  min-height: 670px;
+}
+
+.tabla_ordenes {
+  font-size: 15px;
+  border-collapse: collapse;
+  max-width: 100%;
+  margin: 40px;
+}
+.tabla_ordenes thead {
+  font-weight: bold;
+  text-align: left;
+  color: #007a5e;
+}
+.tabla_ordenes td,
+.tabla_ordenes th {
+  padding: 5px 10px;
+  text-align: left;
+}
+
+.tabla_ordenes tbody tr:hover {
+  background-color: white;
+  color: #2c3e50;
+  font-weight: bold;
+}
+.tabla_ordenes td {
+  border: 1px solid;
+}
+</style>
